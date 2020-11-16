bb2bc9e9e9de4bd3583b2fa92b57dc474b270b5c
Ezra Tassone <etassone@gmail.com>
2020-11-16 08:47:43 +0800

    Updated app.py for Flask API section

--- app.py
+++ app.py
@@ -56,19 +56,21 @@ def precipitation():
     session = Session(engine)
 
     # Find lastest date from database
-    # results = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
     
-    # Already know last date from previous section
-    date_end = dt.datetime(2017, 8, 23)
+    # Convert date from query above to required datetime object
+    date_time_obj = dt.datetime.strptime(last_date[0],"%Y-%m-%d")
+
+    # Extract date from datetime object
+    date_end = date_time_obj.date()
 
     # Calculate one year before from end date
-    # Set start date one day earlier to capture 2016-08-23
-    date_start = date_end - dt.timedelta(days=366)
+    date_start = date_end - dt.timedelta(days=365)
 
-    # Perform a query to retrieve the date and precipitation values
+    # Perform a query to retrieve the date and precipitation values for date range
     # Order results by date for easier reading
     results = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date > date_start).filter(Measurement.date <= date_end).order_by(Measurement.date).all()
+        filter(Measurement.date >= date_start).filter(Measurement.date <= date_end).order_by(Measurement.date).all()
 
     # Close session to DB
     session.close()
@@ -114,24 +116,88 @@ def tobs():
     # Query counts how many times each station's name appears and sorts in descending order
     station_activity = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
 
-    # Already know last date from previous Jupyter notebook section
-    date_end = dt.datetime(2017, 8, 23)
+    # Find lastest date from database
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    
+    # Convert date from above query to required datetime object
+    date_time_obj = dt.datetime.strptime(last_date[0],"%Y-%m-%d")
+
+    # Extract date from datetime object
+    date_end = date_time_obj.date()
 
     # Calculate one year before from end date
-    # Set start date one day earlier to capture 2016-08-23
-    date_start = date_end - dt.timedelta(days=366)
+    date_start = date_end - dt.timedelta(days=365)
 
     # Station with mosts activity at location [0][0] in station_activity
+    # Retrieve for one year date range
     # Order results by date for easier reading
-    most_active_station_temps = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == station_activity[0][0]).filter(Measurement.date > date_start).filter(Measurement.date <= date_end).order_by(Measurement.date).all()
+    most_active_station_temps = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == station_activity[0][0]).filter(Measurement.date >= date_start).filter(Measurement.date <= date_end).order_by(Measurement.date).all()
 
     # Close session to DB
     session.close()
 
-    # Return the JSON representation of temperaturess with date for most active station
+    # Return the JSON representation of temperatures with date for most active station
     return jsonify(most_active_station_temps)
 
+# Route to min, average and max temperature for a given start date
+# Start date given in YYYY-mm-dd format
+@app.route("/api/v1.0/<start>")
+def min_average_max_tobs(start):
 
-    
+    # Convert date given in API call to required datetime object
+    date_time_obj = dt.datetime.strptime(start,"%Y-%m-%d")
+
+    # Extract date from datetime object
+    start_date = date_time_obj.date()
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Perform a query to retrieve the min, average and max temperature values on any date after the start date inclusive
+    temp_obs = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).all()
+   
+    # Close session to DB
+    session.close()
+
+    # Return the JSON representation of min, average, and max temperature
+    if temp_obs[0][0]:
+        return jsonify(temp_obs)
+
+    # Return 404 error if date not in dataset
+    return jsonify({"error": "Date not found"}), 404
+
+# Route to min, average and max temperature for a given start and end date
+# Start date given in YYYY-mm-dd format
+@app.route("/api/v1.0/<start>/<end>")
+def min_average_max_tobs_range(start, end):
+
+    # Convert dates given in API call to required datetime object
+    date_time_obj1 = dt.datetime.strptime(start,"%Y-%m-%d")
+    date_time_obj2 = dt.datetime.strptime(end,"%Y-%m-%d")
+
+    # Extract start date from datetime object
+    start_date = date_time_obj1.date()
+
+    # Extract end date from datetime object
+    end_date = date_time_obj2.date()
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Perform a query to retrieve the min, average and max temperature values on any date after the start date inclusive
+    temp_obs = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+   
+    # Close session to DB
+    session.close()
+
+    # Return the JSON representation of min, average, and max temperature
+    if temp_obs[0][0]:
+        return jsonify(temp_obs)
+
+    # Return 404 error if date not in dataset
+    return jsonify({"error": "Date not found"}), 404
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
